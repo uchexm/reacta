945662a4bc7442ed3d3bd904f6d6bf60527c3a47
CHRIS <NWAKAUC1@GMAIL.COM>
2023-03-18 11:11:19 +0200

    React hook form

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,14 +6,14 @@ import { yupResolver } from "@hookform/resolvers/yup";
 export const Form = () =>{
 
   const schema = yup.object().shape({
-    fullName: yup.string().required(),
+    fullName: yup.string().required("Full Name is required!"),
     email: yup.string().email().required(),
     age: yup.number().positive().integer().required().min(18),
     password: yup.string().required().password().min(6).max(22),
     confirmPassword: yup.string().oneOf([yup.ref("password"), null]).required(),
   });
 
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, formState: {errors}} = useForm({
     resolver: yupResolver(schema),
   });
 
@@ -23,10 +23,15 @@ export const Form = () =>{
   return(
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="Full Name..." {...register("fullName")}/>
+      <p>{errors.fullName?.message}</p>
       <input type="text" placeholder="Email..." {...register("email")}/>
+      <p>{errors.email?.message}</p>
       <input type="number" placeholder="Age..." {...register("age")} />
+      <p>{errors.age?.message}</p>
       <input type="password" placeholder="Password..." {...register("password")} />
+      <p>{errors.password?.message}</p>
       <input type="password" placeholder="Confirm Password..." {...register("password")}/>
+      <p>{errors.confirmPassword?.message}</p>
       <input type="submit"/>
     </form>
   );
