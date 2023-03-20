3b972707d60a1eb4c0afd9c0c794988bf132fc0c
CHRIS <NWAKAUC1@GMAIL.COM>
2023-03-20 10:53:05 +0200

    React hook form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,21 @@
 import { useForm } from "react-hook-form";
+import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+
+
 export const Form = () =>{
-  const { register, handleSubmit } = useForm();
+
+  const schema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup.string().email().required(),
+    age: yup.number().positive().integer().required().min(18),
+    password: yup.string().required().password().min(6).max(22),
+    confirmPassword: yup.string().oneOf([yup.ref("password"), null]).required(),
+  });
+
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (data)=>{
     console.log(data);
