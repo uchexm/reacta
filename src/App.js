43df3f99390783f2d564df6935850a24192dc726
CHRIS <NWAKAUC1@GMAIL.COM>
2023-03-26 11:03:43 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,72 +1,31 @@
 import "./App.css";
+import Axios  from "axios";
 import { useState, useEffect } from "react";
-import { Task } from "./Task";
 
-function App(){
-  const [todos, setTodos] = useState([]);
-  const [newTask, setNewTask] = useState("");
+// fetch("https://catfact.ninja/fact").then((res)=>{res.json().then((data)=>{console.log(data);})});*/
 
-  const handleChange = (e) => {
-    setNewTask(e.target.value);
-  };
 
-  const addTodo = () => {
-    const task = {
-      id: todos.length === 0 ? 1 : todos[todos.length - 1].id + 1,
-      taskName: newTask,
-      completed: false,
-    };
-    setTodos([...todos, task])
-  };
+function App() {
+  const [catFact, setCatFact] = useState("");
   
-  const completeTodo = (id) => {
-    setTodos(todos.map((todo)=>{
-      if(todo.id === id){
-        return{...todos, completed: true};
-      }else{
-        return todo;
-      }
-    }))  
+  const fetchFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res)=>{
+    setCatFact(res.data.fact)
+    });
   };
 
-  const deleteTask = (id) => {
-    setTodos(todos.filter((todo)=> todo.id !== id));
-  };
-  
-  const Text = () => {
-    const [text, setText] = useState("");
-    
-    return(
-      <div>
-        <input onChange={(e)=>{setText(e.event.target.value)}}/>
-        <h2>{text}</h2>
-      </div>
-    );
-  };
-  
-  useEffect(()=>{
-    console.log('Componenet mounted');
+  useEffect(() => {
+    fetchFact();
+  }, []);
 
-    return () => {
-      console.log('Component unmounted');
-    }
-  });
   return (
     <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange}/>
-        <button onClick={addTodo}>Add Task</button> 
-        
-        <button>Show Text</button>
-        
-      </div>
-      <div className="list">
-        {todos.map((todo)=>{
-          return <Task taskName={todo.taskName} id={todo.id} deleteTask={deleteTask} completed={todo.completed} completeTodo={completeTodo}/>
-        })}
-      </div>
+      <button>Generate Cat Fact (fetch) </button>
+      <button onClick={fetchFact}>Generate Cat Fact (axios) </button>
+      <p>{catFact}</p>
     </div>
   );
+
 };
 
 export default App;
