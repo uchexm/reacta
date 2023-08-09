f1c656245f2fff270aac746ead2d218ffe8d52c0
CHRIS <NWAKAUC1@GMAIL.COM>
2023-08-09 20:17:49 +0200

    Todo list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import "./App.css";
 import { useState } from "react";
 
-/*function App(){
-  const [colorText, setColorText] = useState("blue");
-  
-  return(
-    <div className="App">
-      <button onClick={() => {
-        setColorText(colorText === "blue" ? "red" : "blue");
-      }}>Show/Hide</button>
-      <h1 style={{color: colorText }}>Hi I am here</h1>
-    </div>
-  )
-};
-export default App;
-*/
 function App(){
-  let [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTodo = () => {
+    const newTodo = [...todos, newTask]
+    setTodos(newTodo);
+  };
+
   return (
     <div className="App">
-      <button onClick={()=>{setCount(count + 1)}}>Increase</button>
-      <button onClick={()=>{setCount(count - 1)}}>Decrease</button>
-      <button onClick={()=>{setCount(count = 0)}}>Reset</button>
-      {count}
+      <div className="addTask">
+        <input onChange={handleChange}/>
+        <button onClick={addTodo}>Add Task</button> 
+      </div>
+      <div className="list">
+        {todos.map((todo)=>{
+          return <h3>{todo}</h3>
+        })}
+      </div>
     </div>
   );
 };
