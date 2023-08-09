52cb00c11856c95cfd6eacc8644d89e1498672ab
CHRIS <NWAKAUC1@GMAIL.COM>
2023-08-09 14:45:04 +0200

    Learn State

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return (
+function App(){
+  const [colorText, setColorText] = useState("blue");
+  
+  return(
     <div className="App">
-      <User name ="Uche" age={33} email="uche@uche.com" />
-      <User name ="Fred" age={44} email="fred@uche.com" />
-      <User name ="qwaza" age={33} email="uche@uche.com" />
-
-      <Job salary={200000} position="Java Dev" company="Netflix"/>
-      <Job salary={100000} position="Js Dev" company="flex"/>
-      <Job salary={300000} position="Ruby Dev" company="Netflix"/>
-    </div>
-  );
-}
-// user component
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <p>{props.email}</p>
+      <button onClick={() => {
+        setColorText(colorText === "blue" ? "red" : "blue");
+      }}>Show/Hide</button>
+      <h1 style={{color: colorText }}>Hi I am here</h1>
     </div>
   )
 };
-
-//job component
-const Job = (props) => {
-  return(
-    <div>
-      <p>{props.salary}</p>
-      <p>{props.position}</p>
-      <p>{props.company}</p>
-    </div>
-  );
-};
-
 export default App;
